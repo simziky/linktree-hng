6bd05d4e43e92c5a84777bfe07d032a4c87f893f
simeon ehioze <simeonehioze@gmail.com>
2022-10-28 22:38:35 +0100

    Link section completed

--- src/pages/home/linkTree.js
+++ src/pages/home/linkTree.js
@@ -31,7 +31,27 @@ function Home() {
 
         </div>
 
+        {/******************************Link Section***********************/}
         <div className='links-section'>
+          <a href="https://twitter.com/Simeon_kong" >Twitter Link</a>
+
+          <a href="https://training.zuri.team/" >Zuri Team</a>
+
+          <a href="http://books.zuri.team" >Zuri Books</a>
+
+          <a href="https://books.zuri.team" >Python Books</a>
+
+          <a href="https://background.zuri.team" >Background Check for Coders</a>
+
+          <a href="https://books.zuri.team/design-rules" >Design Books</a>
+
+        </div>
+
+        <div className="socials">
+
+          <div className="socials-inner">
+
+          </div>
 
         </div>
 
